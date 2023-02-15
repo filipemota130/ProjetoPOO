29c67419de864db9c9e81a87e7ff3ff7f5574278
Sarah Domingos <92494941+sarahdomingos@users.noreply.github.com>
2023-02-14 23:12:00 -0300

    add historico e disciplinas

--- Aluno.java
+++ Aluno.java
@@ -1,214 +1,228 @@
+package aluno;
+
 import java.util.ArrayList;
-import  java.util.Scanner;
+import java.util.Scanner;
 
 public class Aluno {
 
     // Atributos.
     public String nome;
     public int matricula;
-    public String dataNasc;
-    public char sexo;
-    public String etnia;
     public String email;
-    public int cpf;
-    public int rg;
-    public int telefone;
+    public String cpf;
     ArrayList<String> notas = new ArrayList<String>();
-    
+    ArrayList<String> historicoAnalitico = new ArrayList<String>();
+    ArrayList<String> disciplinasAtuais = new ArrayList<String>();
 
-   
     // Cadastrar informações dos alunosCadastrados.
-    public Aluno(String nome, int matricula, String dataNasc, char sexo, String etnia, String email, int cpf, int rg, int telefone) {
+    public Aluno(String nome, int matricula, String email, String cpf) {
         this.nome = nome;
         this.matricula = matricula;
-        this.dataNasc = dataNasc;
-        this.sexo = sexo;
-        this.etnia = etnia;
         this.email = email;
         this.cpf = cpf;
-        this.rg = rg;
-        this.telefone = telefone;
+    }
+
+    public void printDisciplina() {
+        if (disciplinasAtuais.isEmpty()) {
+            System.out.println("O aluno não está matriculado em nenhuma disciplina.");
+        } else {
+            for (int i = 0; i < disciplinasAtuais.size(); i++) {
+                System.out.println(disciplinasAtuais.get(i));
+            }
+        }
+
     }
 
     // Exibir todas as informações.
-    public String printAluno() {
-        return "Nome: " + this.nome +
-                "\nMatrícula: " + this.nome +
-                "\nRG: " + this.rg +
+    public void printAluno() {
+        System.out.print("Nome: " + this.nome +
+                "\nMatrícula: " + this.matricula +
                 "\nCPF: " + this.cpf +
-                "\nData de nascimento: " + this.dataNasc +
-                "\nSexo: " + this.sexo +
-                "\nEtnia: " + this.etnia +
                 "\nE-mail: " + this.email +
-                "\nTelefone: " + this.telefone;
+                "\nDisciplinas matriculadas: ");
+        printDisciplina();
     }
-    
 
-    // Seção de sets.
+    // função boletim
+    public void printBoletim() {
+        for (int i = 0; i < notas.size(); i++) {
+            System.out.println(notas.get(i));
+        }
+    }
+
+    public void printHistorico() {
+        for (int i = 0; i < historicoAnalitico.size(); i++) {
+            System.out.println(historicoAnalitico.get(i));
+        }
+    }
+
     public void setNomeAluno(String nome) {
         this.nome = nome;
     }
-    public void setMatriculaAluno(int matricula) {
-        this.matricula = matricula;
-    }
-    public void setRgAluno(int rg) {
-        this.rg = rg;
-    }
-    public void setCpfAluno(int cpf) {
-        this.cpf = cpf;
-    }
-    public void setSexoAluno(char sexo) {
-        this.sexo = sexo;
-    }
-    public void setEtniaAluno(String etnia) {
-        this.etnia = etnia;
-    }
-    public void setDataNascAluno(String dataNasc) {
-        this.dataNasc = dataNasc;
-    }
-    public void setEmailAluno(String email) {
-        this.email = email;
-    }
-    public void setTelefoneAluno(int telefone) {
-        this.telefone = telefone;
-    }
-    
-    // Seção de gets.
+
     public String getNomeAluno() {
         return nome;
     }
+
+    public void setMatriculaAluno(int matricula) {
+        this.matricula = matricula;
+    }
+
     public int getMatriculaAluno() {
         return matricula;
     }
-    public int getRgAluno() {
-        return rg;
+
+    public void setCpfAluno(String cpf) {
+        this.cpf = cpf;
     }
-    public int getCpfAluno() {
+
+    public String getCpfAluno() {
         return cpf;
     }
-    public int getTelefoneAluno() {
-        return telefone;
+
+    public void setEmailAluno(String email) {
+        this.email = email;
     }
-    public char getSexoAluno() {
-        return sexo;
-    }
+
     public String getEmailAluno() {
         return email;
     }
-    public String getEtniaAluno() {
-        return etnia;
-    }
-    public String getDataNascAluno() {
-        return dataNasc;
-    }
+
     public static void main(String[] args) {
         ArrayList<Aluno> alunosCadastrados = new ArrayList<Aluno>();
-        Scanner leitor = new Scanner(System.in);
-        int x=0;
+        Aluno alunoT = new Aluno("nome", 1, "email", "79589485");
+        alunosCadastrados.add(alunoT);
+        int x = 0;
 
-        while (x != 5){
+        while (true) {
+            Scanner leitor = new Scanner(System.in);
             System.out.println("\n[1] - Cadastrar Aluno");
             System.out.println("[2] - Remover Aluno");
             System.out.println("[3] - Editar dados do Aluno");
             System.out.println("[4] - Ver informações do Aluno");
-            System.out.println("[5] - Sair");
+            System.out.println("[5] - Gerar Boletim");
+            System.out.println("[6] - Gerar Histórico Analítico");
+            System.out.println("[7] - Matricular Aluno em disciplina");
+            System.out.println("[8] - Sair");
             x = leitor.nextInt();
 
-            if (x == 1){
-                /*
-                System.out.print("\nNome: ");
-                String nome = leitor.nextLine();
-    
-                System.out.print("\nMatrícula: ");
+            if (x == 1) {
+                System.out.print("Nome: ");
+                String nome = leitor.next();
+
+                System.out.print("Matrícula: ");
                 int matricula = leitor.nextInt();
-    
-                System.out.print("\nCPF: ");
-                int cpf = leitor.nextInt();
-    
-                System.out.print("\nRG: ");
-                int rg = leitor.nextInt();
-    
-                System.out.print("\nData de nascimento: ");
-                String dataNasc = leitor.nextLine();
-    
-                System.out.print("\nSexo: ");
-                char sexo = leitor.nextLine().charAt(0);
-    
-                System.out.print("\nEtnia: ");
-                String etnia = leitor.nextLine();
-    
-                System.out.print("\nTelefone: ");
-                int telefone = leitor.nextInt();
-    
-                System.out.print("\nEmail: ");
-                String email = leitor.nextLine(); */
-    
-                Aluno aluno1 = new Aluno("nome", 1234, "dataNasc", 'F', "etnia", "email", 1235, 678, 678768);
+
+                System.out.print("CPF: ");
+                String cpf = leitor.next();
+
+                System.out.print("Email: ");
+                String email = leitor.next();
+
+                Aluno aluno1 = new Aluno(nome, matricula, email, cpf);
                 alunosCadastrados.add(aluno1);
                 System.out.println("Aluno adicionado!\n");
-                System.out.println(aluno1.printAluno());
-            }
-            
-            if (x == 2) {
-                System.out.println("ID do aluno: ");
-                int id = leitor.nextInt();
-                alunosCadastrados.remove(id);
-                System.out.println("Aluno removido!\n");
-            }
-    
-            if (x == 3) {
-                System.out.println("ID do aluno: ");
-                int id = leitor.nextInt();
-                System.out.println("1 - Editar telefone");
-                System.out.println("2 - Editar email");
-                int aux = leitor.nextInt();
-                if (aux == 1) {
-                    System.out.println("Digite o novo telefone: ");
-                    int telefone = leitor.nextInt();
-                    Aluno aluno1 = alunosCadastrados.get(id);
-                    aluno1.setTelefoneAluno(telefone);
-                    System.out.println("Iformação atualizada!\n");
-
-                }
-                if (aux == 2) {
-                    System.out.println("Digite o novo email: ");
-                    String email = leitor.nextLine();
-                    Aluno aluno1 = alunosCadastrados.get(id);
-                    aluno1.setEmailAluno(email);
-                    System.out.println("Iformação atualizada!\n");
-                }
-
-            }
-
-            if (x == 4) {
-                System.out.println("ID do aluno: ");
-                int id = leitor.nextInt();
-                Aluno aluno1 = alunosCadastrados.get(id);
-                System.out.println(aluno1.printAluno());
-                
-            }
-            if (x == 5){
-                System.out.println("Insira o nome do aluno desejado");
-                String aluno_name = leitor.next();
-                for (int i=0;i<alunosCadastrados.size();i++){
-                    if (alunosCadastrados.get(i).nome.equals(aluno_name)){
-                        System.out.println("Boletim do aluno" + alunosCadastrados.get(i).nome);
-                        alunosCadastrados.get(i).notas.add("Algebra --- 4.6");
-                        alunosCadastrados.get(i).notas.add("logica 1 --- 7.6");
-                        alunosCadastrados.get(i).notas.add("Programacao 1 --- 8.6");
-                        alunosCadastrados.get(i).notas.add("Teoria da Computacao --- 9.6");
-                        alunosCadastrados.get(i).notas.add("Sistemas de Informacao --- 10.0");
-                        alunosCadastrados.get(i).notas.add("Calculo3 --- 5.6");
-                        //alunosCadastrados.get(i).printBoletim();
+                aluno1.printAluno();
+            } else if (x == 2) {
+                System.out.print("Matrícula do aluno: ");
+                int matricula = leitor.nextInt();
+                for (int i = 0; i < alunosCadastrados.size(); i++) {
+                    if (alunosCadastrados.get(i).matricula == matricula) {
+                        alunosCadastrados.remove(i);
+                        System.out.println("Aluno removido!\n");
                         break;
                     }
                 }
-      }
+            } else if (x == 3) {
+                System.out.print("Matrícula do aluno: ");
+                int matricula = leitor.nextInt();
+                for (int i = 0; i < alunosCadastrados.size(); i++) {
+                    if (alunosCadastrados.get(i).matricula == matricula) {
+                        alunoT = alunosCadastrados.get(i);
+                        break;
+                    }
+                }
+                System.out.println("[0] - Editar nome");
+                System.out.println("[1] - Editar email");
+                int aux = leitor.nextInt();
+                if (aux == 0) {
+                    System.out.print("Digite o novo nome: ");
+                    String new_name = leitor.next();
+                    alunoT.setNomeAluno(new_name);
+                    System.out.println("Informação atualizada!\n");
+                } else if (aux == 1) {
+                    System.out.print("Digite o novo email: ");
+                    String email = leitor.next();
+                    alunoT.setEmailAluno(email);
+                    System.out.println("Informação atualizada!\n");
+                }
+            } else if (x == 4) {
+                System.out.print("Matrícula do aluno: ");
+                int matricula = leitor.nextInt();
+                for (int i = 0; i < alunosCadastrados.size(); i++) {
+                    if (alunosCadastrados.get(i).matricula == matricula) {
+                        alunosCadastrados.get(i).printAluno();
+                        break;
+                    }
+                }
+            } else if (x == 5) {
+                System.out.print("Matrícula do aluno: ");
+                int matricula = leitor.nextInt();
+                for (int i = 0; i < alunosCadastrados.size(); i++) {
+                    if (alunosCadastrados.get(i).matricula == matricula) {
+                        System.out.println("Boletim do aluno " + alunosCadastrados.get(i).nome);
+                        alunosCadastrados.get(i).notas.add("Algebra --- 4.6 --- 72h");
+                        alunosCadastrados.get(i).notas.add("logica 1 --- 7.6 --- 72h");
+                        alunosCadastrados.get(i).notas.add("Programacao 1 --- 8.6 --- 144h");
+                        alunosCadastrados.get(i).notas.add("Teoria da Computacao --- 9.6 --- 72h");
+                        alunosCadastrados.get(i).notas.add("Sistemas de Informacao --- 10.0 --- 72h");
+                        alunosCadastrados.get(i).notas.add("Calculo3 --- 5.6 --- 72h");
+                        alunosCadastrados.get(i).printBoletim();
+                        break;
+                    }
+                }
+
+            } else if (x == 6) {
+                System.out.print("Matrícula do aluno: ");
+                int matricula = leitor.nextInt();
+                for (int i = 0; i < alunosCadastrados.size(); i++) {
+                    if (alunosCadastrados.get(i).matricula == matricula) {
+                        System.out.println("Histórico do aluno " + alunosCadastrados.get(i).nome);
+                        alunosCadastrados.get(i).historicoAnalitico.add("Carga Horária Cumprida --- 720h");
+                        alunosCadastrados.get(i).historicoAnalitico.add("Coeficiente de Rendimento --- 82.48%");
+                        alunosCadastrados.get(i).historicoAnalitico.add("1º período --- Programação 1 --- 7.3 --- AP");
+                        alunosCadastrados.get(i).historicoAnalitico
+                                .add("1º período --- Matemática Discreta --- 9.4 --- AP");
+                        alunosCadastrados.get(i).historicoAnalitico.add("2º período --- Banco de Dados --- 10 --- AP");
+                        alunosCadastrados.get(i).historicoAnalitico
+                                .add("2º período --- Estrutura de Dados --- 9.4 --- AP");
+                        alunosCadastrados.get(i).historicoAnalitico.add("3º período --- Àlgebra Linear --- 5.7 --- AP");
+                        alunosCadastrados.get(i).historicoAnalitico
+                                .add("3º período --- Teoria dos Grafos --- 3.1 --- REP");
+                        alunosCadastrados.get(i).historicoAnalitico
+                                .add("Eletiva --- Sistemas Distribídos --- 9.2 --- AP");
+                        alunosCadastrados.get(i).printHistorico();
+                        break;
+                    }
+                }
+            } else if (x == 7) {
+                System.out.print("Matrícula do aluno: ");
+                int matricula = leitor.nextInt();
+                System.out.print("Insira o nome da disciplina: ");
+                String disciplina = leitor.next();
+                for (int i = 0; i < alunosCadastrados.size(); i++) {
+                    if (alunosCadastrados.get(i).matricula == matricula) {
+                        alunosCadastrados.get(i).disciplinasAtuais.add(disciplina);
+                        System.out.println("Disciplina inserida com sucesso!");
+                        break;
+                    }
+                }
+
+            } else if (x == 8) {
+                break;
+            } else {
+                System.out.println("Opção não reconhecida, tente novamente.");
+            }
         }
-        
-        
-
-        leitor.close();
     }
-
 }
